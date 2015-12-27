6516c9c46b619bb40157f19891895fc9eb48e527
putheakhem <putheakhemdeveloper@gmail.com>
2015-12-27 21:02:23 +0700

    init install terminator and zsh

---
+++ terminator-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# Terminator with zsh Configuration by Khem Puthea
+#
+# It can be executed several times e.g. to update the files
+#
+
+echo "Author Mr Khem Puthea"
+echo "Software Developer"
+
+# Installation termintor 
+
+sudo apt-get install -y terminator
+
+# Installation zsh 
+
+sudo apt-get install -y zsh 
+
+
+# clone oh-my-zsh from putheakhem repository 
+echo "cloning into $HOME
+git clone git@github.com:putheakhem/oh-my-zsh.git ~/.oh-my-zsh
+
+
